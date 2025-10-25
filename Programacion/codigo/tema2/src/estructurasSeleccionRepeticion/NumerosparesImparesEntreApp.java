0cd5788e883e68629c14fc473b59624d5bf8ef4e
Rimantico <jgomrui1410@gmail.com>
2025-10-25 18:27:12 +0200

    Prueba

---
+++ Programacion/codigo/tema2/src/estructurasSeleccionRepeticion/NumerosparesImparesEntreApp.java
@@ -0,0 +1,14 @@
+package estructurasSeleccionRepeticion;
+
+import java.util.Scanner;
+
+public class NumerosparesImparesEntreApp {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		sc.close();
+	}
+
+}
